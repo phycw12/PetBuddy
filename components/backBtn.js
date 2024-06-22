a3dce3578fd3080f84f8793aa16a0c7d37dfc0d3
phycw12 <phycw12@gmail.com>
2024-06-22 10:03:25 +0900

    뒤로가기 버튼

---
+++ components/backBtn.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { getStorage, getDownloadURL, ref } from 'firebase/storage';
+import { Back, BackImg } from '@/styles/emotion';
+
+export default function BackBtn() {
+    const [loading, setLoading] = useState(true); // 이미지 로딩 상태
+    const [backImageUrl, setBackImageUrl] = useState('');
+    const router = useRouter();
+    const storage = getStorage();
+
+    useEffect(() => {
+        const fetchImages = async () => {
+            try {
+                const backImageUrl = await getDownloadURL(ref(storage, '/petbuddy/back.svg'));
+                setBackImageUrl(backImageUrl);
+
+                setLoading(false); // 이미지 로딩 완료
+            } catch (error) {
+                console.error('Error fetching images:', error);
+                setLoading(true); // 이미지 로딩 실패
+            }
+        };
+        fetchImages();
+    }, [storage]);
+
+    const backClick = () => {
+        router.back();
+    };
+
+
+    if (loading) {
+        return (
+        <div>Loading...</div>);
+    };
+
+    return(
+        <Back>
+            <BackImg src={backImageUrl} alt="Back" onClick={backClick}/>
+        </Back>
+    );
+};
